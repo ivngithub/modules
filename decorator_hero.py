3aa8ca0e6218c4c5e9df028f74b9ab7e74e43083
ivn <ivn@li.ru>
2019-11-01 17:47:19 +0300

    upgrade and finish

--- decorator_hero.py
+++ decorator_hero.py
@@ -1,22 +1,22 @@
 from abc import ABC, abstractmethod
-
-
-class Creature(ABC):
-
-    @abstractmethod
-    def get_positive_effects(self):
-        pass
-
-    @abstractmethod
-    def get_negative_effects(self):
-        pass
-
-    @abstractmethod
-    def get_stats(self):
-        pass
-
-
-class Hero(Creature):
+#
+#
+# class Creature(ABC):
+#
+#     @abstractmethod
+#     def get_positive_effects(self):
+#         pass
+#
+#     @abstractmethod
+#     def get_negative_effects(self):
+#         pass
+#
+#     @abstractmethod
+#     def get_stats(self):
+#         pass
+#
+#
+class Hero:
     def __init__(self):
         self.positive_effects = []
         self.negative_effects = []
@@ -43,32 +43,49 @@ class Hero(Creature):
         return self.stats.copy()
 
 
-class AbstractEffect(Creature):
+class AbstractEffect(Hero, ABC):
     def __init__(self, base):
+        super(AbstractEffect, self).__init__()
         self.base = base
 
+    @abstractmethod
     def get_positive_effects(self):
-        self.base.get_positive_effects()
+        pass
 
+    @abstractmethod
     def get_negative_effects(self):
-        self.base.get_negative_effects()
+        pass
 
+    @abstractmethod
     def get_stats(self):
         self.base.get_stats()
 
 
-# class AbstractPositive(AbstractEffect):
-#     def get_positive_effects(self):
-#         buff = self.base.get_positive_effects()
-#         buff.append(self.__class__.__name__)
-#         return buff
-#
-#
-# class AbstractNegative(AbstractEffect):
-#     def get_negative_effects(self):
-#         buff = self.base.get_positive_effects()
-#         buff.append(self.__class__.__name__)
-#         return buff
+class AbstractPositive(AbstractEffect):
+    def __init__(self, base):
+        super().__init__(base)
+
+    @abstractmethod
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
+
+
+
+class AbstractNegative(AbstractEffect):
+    def __init__(self, base):
+        super().__init__(base)
+    # @abstractmethod
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+
+    @abstractmethod
+    def get_negative_effects(self):
+        buff = self.base.get_positive_effects()
+        buff.append(self.__class__.__name__)
+        return buff
 
 
 """
@@ -76,7 +93,7 @@ class AbstractEffect(Creature):
 уменьшает характеристики: Восприятие, Харизма, Интеллект на 3;
 количество единиц здоровья увеличивается на 50.
 """
-class Berserk(AbstractEffect):
+class Berserk(AbstractPositive):
     """
         "Strength": 15,  # сила
         "Perception": 4,  # восприятие
@@ -87,12 +104,9 @@ class Berserk(AbstractEffect):
         "Luck": 1
     """
 
-    def get_negative_effects(self):
-        return self.base.get_negative_effects()
-
     def get_positive_effects(self):
         buff = self.base.get_positive_effects()
-        buff.append('Berserk')
+        buff.append(self.__class__.__name__)
         return buff
 
     def get_stats(self):
@@ -112,12 +126,11 @@ class Berserk(AbstractEffect):
 """
 увеличивает все основные характеристики на 2.
 """
-class Blessing(AbstractEffect):
-    def get_negative_effects(self):
-        return self.base.get_negative_effects()
+class Blessing(AbstractPositive):
+
     def get_positive_effects(self):
         buff = self.base.get_positive_effects()
-        buff.append('Blessing')
+        buff.append(self.__class__.__name__)
         return buff
 
     def get_stats(self):
@@ -131,23 +144,10 @@ class Blessing(AbstractEffect):
         buff['Luck'] += 2
         return buff
 
-def start():
-    hero = Hero()
-    brs1 = Berserk(hero)
-    print(brs1.get_stats())
-    print(brs1.get_negative_effects())
-    print(brs1.get_positive_effects())
-    brs2 = Berserk(brs1)
-    bls1 = Blessing(brs2)
-    print(bls1.get_stats())
-    print(bls1.get_negative_effects())
-    print(bls1.get_positive_effects())
-    return brs1, brs2, bls1
-
 """
 уменьшает характеристики: Сила, Выносливость, Ловкость на 4
 """
-class Weakness(AbstractEffect):
+class Weakness(AbstractNegative):
     """
         "Strength": 15,  # сила
         "Perception": 4,  # восприятие
@@ -157,12 +157,10 @@ class Weakness(AbstractEffect):
         "Agility": 8,  # ловкость
         "Luck": 1
     """
-    def get_positive_effects(self):
-        return self.base.get_positive_effects()
 
     def get_negative_effects(self):
-        buff = self.base.get_positive_effects()
-        buff.append('Weakness')
+        buff = self.base.get_negative_effects()
+        buff.append(self.__class__.__name__)
         return buff
 
     def get_stats(self):
@@ -175,14 +173,11 @@ class Weakness(AbstractEffect):
 """
 уменьшает все основные характеристики на 2.
 """
-class Curse(AbstractEffect):
-
-    def get_positive_effects(self):
-        return self.base.get_positive_effects()
+class Curse(AbstractNegative):
 
     def get_negative_effects(self):
-        buff = self.base.get_positive_effects()
-        buff.append('Curse')
+        buff = self.base.get_negative_effects()
+        buff.append(self.__class__.__name__)
         return buff
 
     def get_stats(self):
@@ -199,14 +194,11 @@ class Curse(AbstractEffect):
 """
 уменьшает  характеристику Удача на 10.
 """
-class EvilEye(AbstractEffect):
-
-    def get_positive_effects(self):
-        return self.base.get_positive_effects()
+class EvilEye(AbstractNegative):
 
     def get_negative_effects(self):
-        buff = self.base.get_positive_effects()
-        buff.append('EvilEye')
+        buff = self.base.get_negative_effects()
+        buff.append(self.__class__.__name__)
         return buff
 
     def get_stats(self):
