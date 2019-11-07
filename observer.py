43dd94d5ae2e3bef93135cd195a21f2d47c55e24
ivn <ivn@li.ru>
2019-11-07 16:44:57 +0300

    pattern observer finished

--- observer.py
+++ observer.py
@@ -1,25 +1,47 @@
 from abc import ABC, abstractmethod
 
 
-class Engine:
-    pass
+# class Engine:
+#     pass
+
+
+class ObservableEngine(Engine):
+    def __init__(self):
+        self.__subscribers = set()  # При инициализации множество подписчиков задается пустым
+
+    def subscribe(self, subscriber):
+        self.__subscribers.add(
+            subscriber)  # Для того чтобы подписать пользователя, он добавляется во множество подписчиков
+
+    def unsubscribe(self, subscriber):
+        self.__subscribers.remove(subscriber)  # Удаление подписчика из списка
+
+    def notify(self, message):
+        for subscriber in self.__subscribers:
+            subscriber.update(message)  # Отправка уведомления всем подписчикам
 
 
 class AbstractObserver(ABC):
     @abstractmethod
-    def update(self):
+    def update(self, achievement):
         pass
 
-
-class ObservableEngine(Engine):
-    pass
-
+"""
+{"title": "Покоритель", "text": "Дается при выполнении всех заданий в игре"}
+"""
 
 class ShortNotificationPrinter(AbstractObserver):
-    def update(self):
-        pass
+    def __init__(self):
+        self.achievements = set()
+
+    def update(self, achievement):
+        self.achievements.add(achievement['title'])
 
 
 class FullNotificationPrinter(AbstractObserver):
-    def update(self):
-        pass
+    def __init__(self):
+        self.achievements = list()
+
+    def update(self, achievement):
+        if achievement not in self.achievements:
+            self.achievements.append(achievement)
