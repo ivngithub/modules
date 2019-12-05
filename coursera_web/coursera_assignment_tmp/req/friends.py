c0a28431da9a48fca566961fbe5133d0ee16295e
ivn <ivn@li.ru>
2019-12-05 16:03:35 +0300

    start coursera web

---
+++ coursera_web/coursera_assignment_tmp/req/friends.py
@@ -0,0 +1,17 @@
+ACCESS_TOKEN = '17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711'
+
+"""
+https://api.vk.com/method/users.get?v=5.71&access_token=17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711&user_ids=id90437796
+"""
+
+
+"""
+https://api.vk.com/method/friends.get?v=5.71&access_token=17da724517da724517da72458517b8abce117da17da72454d235c274f1a2be5f45ee711&user_id=90437796&fields=bdate
+"""
+def calc_age(uid):
+    pass
+
+
+if __name__ == '__main__':
+    res = calc_age('reigning')
+    print(res)
