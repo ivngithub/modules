b274ef8d377b8a504ce5cedf2327e7fc9fe2ba12
ivn <ivn@li.ru>
2020-04-28 16:00:26 +0300

    week2 finished

--- coursera_web/wikistat.py
+++ coursera_web/wikistat.py
@@ -119,19 +119,119 @@ def parse(start, end, path):
         imgs = len(list(filter(lambda img: img.attrs.get("width") and
                                            int(img.attrs.get("width")) >= 200, body.findAll("img"))))  # Количество картинок (img) с шириной (width) не меньше 200
 
-        headers = len(body.findAll(re.compile(r"[hH]\d")))  # Количество заголовков, первая буква текста внутри которого: E, T или C
-        linkslen = body.findAll("a")  # TODO 16042020 Длина максимальной последовательности ссылок, между которыми нет других тегов
+        headers = len(list(filter(lambda h: h.text[0].upper() in ('E', 'T', 'C'),
+                                  [h for h in body.findAll(re.compile(r"[hH]\d")) if h.text])))  # Количество заголовков, первая буква текста внутри которого: E, T или C
+        # linkslen = body.findAll("a")  # TODO 16042020 Длина максимальной последовательности ссылок, между которыми нет других тегов
 
-        lists = len(body.findAll("ul"))  # Количество списков, не вложенных в другие списки
+        tag_a = body.find_next("a")
 
-        out[file] = [imgs, headers, [(item, link) for item, link in enumerate(linkslen)], lists]
+        best_result = 0
+        while tag_a:
+            pre_result = 1
+            for a in tag_a.find_next_siblings():
+                if a.name != 'a':
+                    break
+                pre_result += 1
+            if pre_result > best_result:
+                best_result = pre_result
+
+            tag_a = tag_a.find_next("a")
+            if not tag_a:
+                break
+
+        lists = body.findAll(["ol", "ul"]) # Количество списков, не вложенных в другие списки
+        best_result_lists = 0
+        for i in lists:
+
+            parent_tags = [t.name for t in i.find_parents()]
+            if 'ul' not in parent_tags and 'ol' not in parent_tags:
+                best_result_lists += 1
+
+        out[file] = [imgs, headers, best_result, best_result_lists]
 
     return out
 
 
 if __name__ == '__main__':
-    print('start ...')
-    # print(build_bridge('Stone_Age', 'Python_(programming_language)', 'wiki/'))
-    print(parse('Stone_Age', 'Python_(programming_language)', 'wiki/'))
-    # r = build_tree('Structural_geology', 'Python_(programming_language)', 'wiki/')
-    print('finish ...')
+    # print('start ...')
+    # # print(build_bridge('Stone_Age', 'Python_(programming_language)', 'wiki/'))
+    # print(parse('Stone_Age', 'Python_(programming_language)', 'wiki/'))
+    # # r = build_tree('Structural_geology', 'Python_(programming_language)', 'wiki/')
+    # print('finish ...')
+
+
+    with open("{}{}".format('wiki/', 'Python_(programming_language)')) as f:
+
+        soup = BeautifulSoup(f, "lxml")
+        body = soup.find(id="bodyContent")
+        lists = body.findAll(["ol", "ul"])
+        print(len(lists))
+        # r = lists[0].find_parents()
+        best_result = 0
+        for k, i in enumerate(lists):
+            # print('=S' * 10)
+            # print(i.text)
+            parent_tags = [t.name for t in i.find_parents()]
+            print(parent_tags)
+            if 'ul' not in parent_tags and 'ol' not in parent_tags:
+                best_result += 1
+            # print('=E' * 10)
+        print(best_result)
+            # for elin i.find_parents():
+            #     if el.name == 'ul':
+            #         if el in lists:
+            #             best_result += 1
+            #             break
+            #             print(k, el)
+        # print(k)
+        # print(len(lists)-best_result)
+        # best_result = 0
+        # for el in lists[:1]:
+        #     pre_lists = el.findAllPrevious("ul")
+        #     print(len(pre_lists))
+        #     for item in pre_lists:
+        #         if item in lists:
+        #             print(el)
+        #             best_result += 1
+        #             break
+        # print(len(lists))
+        # print(best_result)
+    # with open("{}{}".format('wiki/', 'Stone_Age')) as f:
+    #
+    #     soup = BeautifulSoup(f, "lxml")
+    #     body = soup.find(id="bodyContent")
+    #
+    #     tag_a = body.find_next("a")
+    #
+    #     best_result = 0
+    #     while tag_a:
+    #         pre_result = 1
+    #         for a in tag_a.find_next_siblings():
+    #             if a.name != 'a':
+    #                 break
+    #             pre_result += 1
+    #         if pre_result > best_result:
+    #             best_result = pre_result
+    #
+    #         tag_a = tag_a.find_next("a")
+    #         if not tag_a:
+    #             break
+    #     print(best_result)
+    #
+    # with open("{}{}".format('wiki/', 'Stone_Age')) as f:
+    #     soup = BeautifulSoup(f, "lxml")
+    #     body = soup.find(id="bodyContent")
+    #
+    #     tag = body.find_next("a")
+    #     linkslen = -1
+    #     while (tag):
+    #         curlen = 1
+    #         for tag in tag.find_next_siblings():
+    #             if tag.name != 'a':
+    #                 break
+    #             curlen += 1
+    #         if curlen > linkslen:
+    #             linkslen = curlen
+    #         tag = tag.find_next("a")
+    #     print(linkslen)
+
